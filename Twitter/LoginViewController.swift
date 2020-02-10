35fcca5aa684ec053b43fc65ab3126ff5b1f4e7f
Hugh Bromund <hughbromund44@gmail.com>
2020-02-09 20:26:09 -0500

    implemented the tweet timeline

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,10 +16,18 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLogedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
     @IBAction func onLoginButton(_ sender: Any) {
         let requestTokenURL = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: requestTokenURL, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLogedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
             
         }, failure: { (Error) in
